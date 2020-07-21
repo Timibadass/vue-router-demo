5d2a16e242bcaea2ef0aaab44c7937387736b032
timibadass <omoyenitimi@gmail.com>
2020-07-21 22:18:10 +0100

    setup homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<p v-if="loading" class="post--empty">Loading....</p>
+		<ol v-else>
+			<li v-for="post in posts" :key="post.id">
+				<router-link
+					:to="{ name: 'Post', params: { id: post.id, post: post } }"
+				>
+					{{ post.title }}
+				</router-link>
+			</li>
+		</ol>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+	// @ is an alias to /src
+	import axios from "axios";
+	export default {
+		name: "Home",
+		data() {
+			return {
+				posts: null,
+				loading: false,
+			};
+		},
+		mounted() {
+			this.getPosts();
+		},
+		methods: {
+			async getPosts() {
+				this.loading = true;
+				try {
+					let res = await axios({
+						url: "https://jsonplaceholder.typicode.com/posts",
+						method: "GET",
+					});
+					let posts = res.data;
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+					this.posts = posts;
+					this.loading = false;
+				} catch (error) {
+					this.loading = false;
+				}
+			},
+		},
+	};
 </script>
+<style>
+	.home {
+		padding: 0 30px;
+		max-width: 800px;
+		margin: 0 auto;
+	}
+
+	@keyframes blink {
+		from {
+			opacity: 1;
+		}
+		to {
+			opacity: 0;
+		}
+	}
+	.post--empty {
+		height: 250px;
+		margin-top: 30px;
+		animation: blink 0.8s ease-in-out infinite alternate both;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-family: "Lobster", cursive;
+	}
+
+	ul {
+		text-align: left;
+	}
+
+	a {
+		color: inherit;
+	}
+</style>
