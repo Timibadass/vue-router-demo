76217a611da7bd18ed039af2ad3817b405d7d5b2
timibadass <omoyenitimi@gmail.com>
2020-07-21 13:28:27 +0100

    add post page

---
+++ src/views/Post.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="about">
+		<div class="post">
+			<h1>{{ post.title }}</h1>
+			<p v-html="post.body"></p>
+		</div>
+		<p>End of page</p>
+	</div>
+</template>
+<script>
+	export default {
+		name: "Post",
+		data() {
+			return {
+				post: this.$route.post,
+			};
+		},
+		props: ["id", "post"],
+	};
+</script>
+<style>
+	.about {
+		background-color: lightpink;
+	}
+	.post {
+		padding: 0 30px;
+		height: 110vh;
+		margin: 0 auto;
+	}
+
+	p {
+		margin: 10px 0;
+	}
+</style>
