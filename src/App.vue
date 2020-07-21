9ec3dbf62f5ee5d31fe0fd46ef58ecbddcfc6d7e
timibadass <omoyenitimi@gmail.com>
2020-07-21 15:50:06 +0100

    add general route transition to app

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
 		<div id="nav">
 			<router-link to="/">Home</router-link>
 		</div>
-		<router-view />
+		<transition name="slide-fade">
+			<router-view />
+		</transition>
 	</div>
 </template>
 
@@ -27,5 +29,21 @@
 
 	#nav a.router-link-exact-active {
 		color: #42b983;
+	}
+	.slide-fade-enter-active {
+		transition: transform 0.3s cubic-bezier(1, 0.5, 0.8, 1),
+			color 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+	.slide-fade-leave-active {
+		transition: transform 1s cubic-bezier(1, 0.5, 0.8, 1),
+			color 1s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+	.slide-fade-enter {
+		color: mediumblue;
+		transform: translateY(20px);
+	}
+	.slide-fade-leave-to {
+		transform: translateX(100px);
+		color: cyan;
 	}
 </style>
