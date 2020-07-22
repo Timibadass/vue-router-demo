f7151a8be3d1959da886833fb93d7ef859b4cc2a
timibadass <omoyenitimi@gmail.com>
2020-07-22 11:34:35 +0100

    set up dynamic transition name

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,26 @@
 		<div id="nav">
 			<router-link to="/">Home</router-link>
 		</div>
-		<transition name="slide-fade">
+		<transition :name="transitionName">
 			<router-view />
 		</transition>
 	</div>
 </template>
-
+<script>
+	export default {
+		data() {
+			return {
+				transitionName: "slide-fade",
+			};
+		},
+		watch: {
+			$route(to, from, params) {
+				const toParam = to.params && to.params.id ? to.params.id : 0;
+				this.transitionName = toParam % 2 === 0 ? "slide-left" : "slide-fade";
+			},
+		},
+	};
+</script>
 <style>
 	#app {
 		font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -30,6 +44,7 @@
 	#nav a.router-link-exact-active {
 		color: #42b983;
 	}
+	/* add transition styles */
 	.slide-fade-enter-active {
 		transition: transform 0.3s cubic-bezier(1, 0.5, 0.8, 1),
 			color 0.5s cubic-bezier(1, 0.5, 0.8, 1);
@@ -44,6 +59,23 @@
 	}
 	.slide-fade-leave-to {
 		transform: translateX(100px);
+		color: cyan;
+	}
+
+	.slide-left-enter-active {
+		transition: transform 0.3s cubic-bezier(1, 0.5, 0.8, 1),
+			color 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+	.slide-left-leave-active {
+		transition: transform 1s cubic-bezier(1, 0.5, 0.8, 1),
+			color 1s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+	.slide-left-enter {
+		color: mediumblue;
+		transform: translateY(20px);
+	}
+	.slide-left-leave-to {
+		transform: skewY(90deg);
 		color: cyan;
 	}
 </style>
